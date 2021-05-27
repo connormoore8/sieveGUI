2fad7b1ba2b8fc8999ca3f1e263c1088ad950ff1
Connor Moore <connormoore8@gmail.com>
2021-05-26 22:57:18 -0500

    added to app, starting organization and starting display boxes.

--- main.py
+++ main.py
@@ -3,37 +3,45 @@ from tkinter import *
 import tkinter as tk
 
 def sieve(X, n):
-    return np.setdiff1d(X,[n**2 + n*y for y in range(0,len(X)//n)])
+    return np.setdiff1d(X, [n**2 + n*y for y in range(0, X[-1])])
 
-def print_result(result):
-    print(e1.get())
-    if e1.get()!='':
-        N = int(e1.get())
-        #print(type(N))
-        if type(N) is int:
 
-            X = np.linspace(2, N, N - 1, dtype=int)
-            for x in X:
-                X = sieve(X, x)
-            print(X)
-            result.config(text = str(X))
+
+class App(tk.Tk):
+    def __init__(self, root):
+        self.root = root
+        root.title('Sieve of Eratosthenes')
+        root.geometry('800x800')
+        self.label = Label(root, text="Input Size of List").grid(row=0)
+        self.e1 = Entry(root)
+        self.e1.grid(row=0, column=1)
+        self.label_text = StringVar()
+        self.label_text.set('Awaiting size of Sieve...')
+        self.result = Label(root, textvariable=self.label_text).grid(row=2)
+        self.button = Button(root, text='print sieve', command=self.print_result).grid(row=0, column=2)
+        self.quit_button = Button(root, text='exit', command=quit).grid(row=0, column=3)
+
+    def createDisplay(self, container):
+        return 0
+
+    def print_result(self):
+        print(self.e1.get())
+        if self.e1.get() != '':
+            n = int(self.e1.get())
+            # print(type(N))
+            if type(n) is int:
+                x = np.linspace(2, n, n - 1, dtype=int)
+                for i in x:
+                    x = sieve(x, i)
+                print(x)
+                self.label_text.set(np.array2string(x,separator=','))
 
 
 if __name__ == '__main__':
     #N = int(input("Enter the size of List: "))
     root = Tk()
-    root.geometry('400x400')
-    root.title('Sieve of Eratosthenes')
-    label = Label(root, text="Input Size of List").grid(row=0)
-    e1 = Entry(root)
-    e1.grid(row=0, column=1)
-    result = Label(root, text='Awaiting size of Sieve...')
-    button = Button(root, text='print sieve', command=(lambda:print_result(result))).grid(row=1)
-    quit_button = Button(root, text='exit', command = quit).grid(row=1, column = 1)
-    result.grid(row=2)
-    #button.bind()
+    app = App(root)
     root.mainloop()
-    #tk.mainloop()
 
 
 
